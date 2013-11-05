eed305920bbc6cabba7eab0120d3dd0e5a329c2c
Valdas Žemaitis <valdas@idega.com>
2013-11-05 11:23:49 +0200

    Using merge by default for exisiting entries

--- src/java/is/idega/block/agresso/business/AgressoFinanceBusiness.java
+++ src/java/is/idega/block/agresso/business/AgressoFinanceBusiness.java
@@ -31,28 +31,28 @@ public class AgressoFinanceBusiness extends DefaultSpringBean {
 			String owner, String ticketNumber, String ticketOfficer,
 			String streetName, String streetNumber, String streetDescription,
 			String meterNumber, String invoiceNumber) {
-		
+
 		try {
 			IWTimestamp paymentDate = new IWTimestamp(ticketDate);
 			paymentDate.addDays(14);
-			
+
 			return getAgressoDAO().addFinanceEntryParking("PARKING", user, amount,
 					paymentDate.getTimestamp(),ticketDate, info, registrationNumber, permanentNumber, carType, owner, ticketNumber, ticketOfficer, streetName,
 					streetNumber, streetDescription, meterNumber, invoiceNumber);
 		} catch (Exception e) {
 			getLogger().log(Level.WARNING, "Error creating parking entry! Ticket number: " + ticketNumber, e);
 		}
-		
+
 		return null;
 	}
-	
+
 	public boolean deleteParkingEntry(Long entryID) {
 		try {
 			return getAgressoDAO().deleteFinanceEntry(entryID);
 		} catch (Exception e) {
 			getLogger().log(Level.WARNING, "Error deleting parking entry: " + entryID, e);
 		}
-		
+
 		return false;
 	}
 
@@ -62,9 +62,9 @@ public class AgressoFinanceBusiness extends DefaultSpringBean {
 		}
 		return agressoDAO;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param ticketNumber, required
 	 * @param isProtested, required otherwise isProtested and protestDate will be nulled in the db
 	 * @param rulingResult, if not supplied rulingResult and rulingDate will be nulled.
@@ -82,11 +82,11 @@ public class AgressoFinanceBusiness extends DefaultSpringBean {
 			getLogger().warning("Unable to find entry in DB table '" + AgressoFinanceEntry.ENTITY_NAME + "' by ticket number: " + ticketNumber);
 			return;
 		}
-		
+
 		if (entries.size() > 1) {
 			getLogger().warning("Founf multiple entries for the same ticket number (" + ticketNumber + "): " + entries);
 		}
-		
+
 		try {
 			AgressoFinanceEntry entry = entries.iterator().next();
 			if (isProtested) {
@@ -99,13 +99,17 @@ public class AgressoFinanceBusiness extends DefaultSpringBean {
 				entry.setIsProtested(null);
 				entry.setProtestedDate(null);
 			}
-			
+
 			entry.setRulingResult(status);
 			entry.setRulingResultDate(rullingDate);
 			entry.setRullingPredefinedText(reason);
 			entry.setRullingExplanationText(explanation);
-			
-			getAgressoDAO().persist(entry);
+
+			if (getApplication().getSettings().getBoolean("agresso.merge_existing_entry", Boolean.TRUE)) {
+				getAgressoDAO().merge(entry);
+			} else {
+				getAgressoDAO().persist(entry);
+			}
 		} catch (Exception e) {
 			String message = "Error while updating agresso entry: ticket number: " + ticketNumber + ", status: " + status + ", reason: " + reason +	", explanation: " +
 				explanation + ", rulling date: " + rullingDate;
