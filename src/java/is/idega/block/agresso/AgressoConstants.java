2118a1850a3480a83c802d8bdf80c0b2cba35ae7
Valdas Žemaitis <valdas@idega.com>
2024-01-25 10:16:14 +0200

    New constants

--- src/java/is/idega/block/agresso/AgressoConstants.java
+++ src/java/is/idega/block/agresso/AgressoConstants.java
@@ -2,7 +2,9 @@ package is.idega.block.agresso;
 
 public interface AgressoConstants {
 
-	public static final String	PARKING_CARD_STATUS_SUCCESS = "success",
+	public static final String	PARKING_CARD_STATUS_PENDING = "pending",
+								PARKING_CARD_STATUS_SUCCESS = "success",
+								PARKING_CARD_STATUS_REACTIVATED = "reactivated",
 
 								TICKET_APPEAL_APPROVED = "approved",
 								TICKET_APPEAL_DENIED = "denied";
